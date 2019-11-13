286c63e1f1036b9688025341dbcc52809081e2e7
Kiril Parushev <kiril.parushev@tick42.com>
2019-11-13 16:51:06 +0200

    module X lab 3 complete

--- extension-template/extension.js
+++ extension-template/extension.js
@@ -1,8 +1,3 @@
-let header = document.querySelector('header');
-let title = document.createElement('h1');
-header.parentNode.insertBefore(title, header.nextSibling);
-title.innerText = `I made this! ${(new Date(Date.now())).toLocaleString()}`;
-
 history.pushState = ((f) => function pushState() {
   const ret = f.apply(this, arguments);
   window.dispatchEvent(new Event('pushState'));
@@ -21,6 +16,41 @@ window.addEventListener('popstate', () => {
   window.dispatchEvent(new Event('locationchange'));
 });
 
-window.addEventListener('locationchange', () => {
-  title.innerText = `I made this! ${(new Date(Date.now())).toLocaleString()}`;
+window.addEventListener("locationchange", () => {
+  myCustomFunctionality();
 });
+
+let interval;
+
+window.dispatchEvent(new Event("locationchange"));
+
+function myCustomFunctionality() {
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => {
+    let button = document.querySelector("div.mdc-security-header a");
+    let myCustomButton = document.querySelector('[myCustomButton]')
+    if (button && !myCustomButton) {
+      drawButton(button);
+      clearInterval(interval);
+    }
+  }, 200);
+}
+
+function drawButton(button) {
+  let glueButton = button.cloneNode(true);
+  glueButton.setAttribute("myCustomButton", "true");
+  glueButton.innerText = `Show in Glue`;
+
+  let icon = button.querySelector("svg").cloneNode(true);
+  glueButton.prepend(icon);
+
+  glueButton.addEventListener("click", onGlueButtonClicked);
+  button.parentNode.insertBefore(glueButton, button.nextSibling);
+}
+
+function onGlueButtonClicked(e) {
+  e.preventDefault();
+  console.log("The button was clicked!");
+}
