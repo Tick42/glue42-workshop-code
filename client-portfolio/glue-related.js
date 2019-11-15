3d4c9c0c3e13ac31bbe277889398c446af503e46
Erjan Gavalji <erjan.gavalji@gmail.com>
2019-11-15 10:52:40 +0200

    Reorder scripts for better flow, add button state setter
    Add the Glue4Office dependency
    
    Use async/await instead of promises
    
    Make the excel sync button action comprehendable
    
    Remove the setButtonState method. Instead, import the setButtonAvailability one
    
    Restore the setButtonState method with a slight improvement

--- client-portfolio/glue-related.js
+++ client-portfolio/glue-related.js
@@ -1,11 +1,11 @@
 async function initializeInterop(onUpdatePortfolioHandler) {
   const glue = await Glue();
-    // From a debugging perspective, it is a good practice to add a reference
-    //   to the Glue42 object globally
+  // From a debugging perspective, it is a good practice to add a reference
+  //   to the Glue42 object globally
   window.glue = glue;
 
-    //Once the Glue42 API object is initialized, declare the capability of
-    // displaying a client portfolio
+  //Once the Glue42 API object is initialized, declare the capability of
+  // displaying a client portfolio
   glue.interop.register("BigBank.ShowPortfolio", (data, server) => {
     onUpdatePortfolioHandler(data);
   });
